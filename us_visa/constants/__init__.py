24f8d2c39d24748f288256e609439611bf5dbf0b
Mohammed Asif Ameen Baig <mohammedasifameenbaig684@gmail.com>
2025-09-11 18:16:20 +0530

    data transformation

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -21,6 +21,7 @@ CURRENT_YEAR = date.today().year                                   #created whil
 PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"                #created while writing data validation code
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")           #created while writing data validation code    
 
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -30,9 +31,18 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
+
 """
 Data Validation realted contant start with DATA_VALIDATION VAR NAME
 """
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+
+"""
+Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
